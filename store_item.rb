d14bf9055b66baa6d97ee31f050b867e93b9051b
Dov Bluming <dovybluming@gmail.com>
2017-02-28 10:42:57 -0500

    3_types_of_hashes

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+bike = {"color" => "blue", "size" => "big", "price" => 500}
+scooter = {:color => "white", :size => "small", :price => 80}
+helmet = {color: "red", size: "small", price: 80}
+
+p " #{bike['color']} "
+p " #{scooter[:color]} "
+p " #{helmet[:color]} "
