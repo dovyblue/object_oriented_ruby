6d5de7b59637d2dcec7e76d4c49dcafde0bba3cd
Dov Bluming <dovybluming@gmail.com>
2017-02-28 13:05:10 -0500

    hash in initialize

--- store_item.rb
+++ store_item.rb
@@ -1,38 +1,39 @@
-# # 3 types of hashes
-# bike = {"color" => "blue", "size" => "big", "price" => 500}
-# scooter = {:color => "white", :size => "small", :price => 80}
-# helmet = {color: "red", size: "small", price: 80}
+# # # 3 types of hashes
+# # bike = {"color" => "blue", "size" => "big", "price" => 500}
+# # scooter = {:color => "white", :size => "small", :price => 80}
+# # helmet = {color: "red", size: "small", price: 80}
 
-# p " #{bike['color']} "
-# p " #{scooter[:color]} "
-# p " #{helmet[:color]} "
+# # p " #{bike['color']} "
+# # p " #{scooter[:color]} "
+# # p " #{helmet[:color]} "
 
 # class
 class Store 
   attr_reader :item, :color, :size, :price
   attr_writer :item, :price
   
-  def 
-    initialize(item, color, size, price)
-    @item = item
-    @color = color
-    @size = size
-    @price = price 
+  def initialize(input_hash)
+    @item = input_hash[:item]
+    @color = input_hash[:color]
+    @size = input_hash[:size]
+    @price = input_hash[:price] 
   end
 
   def sentence
-    "the #{size} #{color} #{item} costs $#{price} "
+    "the #{@size} #{@color} #{@item} costs $#{@price} "
   end
 end
 
-item1 = Store.new("bike", "red", "small", 500)
-item2 = Store.new("scooter", "white", "big", 80)
-item3 = Store.new("helmet", "black", "small", 30)
+item1 = Store.new({item: "bike", color: "red", size: "small", price: 500})
+item2 = Store.new({item: "scooter", color: "white", size: "big", price: 80})
+item3 = Store.new({item: "helmet", color: "black", size: "small", price: 30})
 
-# p item1.sentence
-# p item2.sentence
-# p item3.sentence
+p item1.sentence
+p item2.sentence
+p item3.sentence
 
 item1.price = 550
 p item1.sentence
 p item2.color
+
+
